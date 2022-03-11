e8eebd41013621e5cc0da48591c7c09a13e4ebb9
Bandit Kaewnoi <banditk64@nu.ac.th>
2022-03-11 12:44:20 +0700

    Edit reply text

--- src/main/java/com/banditworkshop2/linebot/LineBotController.java
+++ src/main/java/com/banditworkshop2/linebot/LineBotController.java
@@ -76,8 +76,7 @@ public class LineBotController {
         String replyToken = event.getReplyToken();
 
         try {
-            MessageContentResponse response = lineMessagingClient.getMessageContent(
-                    content.getId()).get();
+            MessageContentResponse response = lineMessagingClient.getMessageContent(content.getId()).get();
             DownloadedContent jpg = saveContent("jpg", response);
             DownloadedContent previewImage = createTempFile("jpg");
 
@@ -94,57 +93,7 @@ public class LineBotController {
 
     }
 
-    private void system(String... args) {
-        ProcessBuilder processBuilder = new ProcessBuilder(args);
-        try {
-            Process start = processBuilder.start();
-            int i = start.waitFor();
-            log.info("result: {} => {}", Arrays.toString(args), i);
-        } catch (InterruptedException e) {
-            log.info("Interrupted", e);
-            Thread.currentThread().interrupt();
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
-
-    private static DownloadedContent saveContent(String ext,
-            MessageContentResponse response) {
-        log.info("Content-type: {}", response);
-        DownloadedContent tempFile = createTempFile(ext);
-        try (OutputStream outputStream = Files.newOutputStream(tempFile.path)) {
-            ByteStreams.copy(response.getStream(), outputStream);
-            log.info("Save {}: {}", ext, tempFile);
-            return tempFile;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
-
-    private static DownloadedContent createTempFile(String ext) {
-        String fileName = LocalDateTime.now() + "-"
-                + UUID.randomUUID().toString()
-                + "." + ext;
-        Path tempFile = Application.downloadedContentDir.resolve(fileName);
-        tempFile.toFile().deleteOnExit();
-        return new DownloadedContent(tempFile,
-                createUri("/downloaded/" + tempFile.getFileName()));
-
-    }
-
-    private static String createUri(String path) {
-        return ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path(path).toUriString();
-    }
-
-    @Value
-    public static class DownloadedContent {
-        Path path;
-        String uri;
-    }
-
-    private void handleTextContent(String replyToken, Event event,
-            TextMessageContent content) {
+    private void handleTextContent(String replyToken, Event event, TextMessageContent content) {
         String text = content.getText();
 
         log.info("Got text message from %s : %s", replyToken, text);
@@ -160,20 +109,22 @@ public class LineBotController {
                                     return;
                                 }
                                 this.reply(replyToken, Arrays.asList(
-                                        new TextMessage("Display name: " +
-                                                profile.getDisplayName()),
-                                        new TextMessage("Status message: " +
-                                                profile.getStatusMessage()),
-                                        new TextMessage("User ID: " +
-                                                profile.getUserId())));
+                                        new TextMessage("Display name: " + profile.getDisplayName()),
+                                        new TextMessage("Status message: " + profile.getStatusMessage()),
+                                        new TextMessage("User ID: " + profile.getUserId())));
                             });
                 }
                 break;
             }
             default:
                 log.info("Return echo message %s : %s", replyToken, text);
-                this.replyText(replyToken, "From Bot:" + text);
+                this.replyText(replyToken, text);
         }
+    }
+
+    private void handleStickerContent(String replyToken, StickerMessageContent content) {
+        reply(replyToken, new StickerMessage(
+                content.getPackageId(), content.getStickerId()));
     }
 
     private void replyText(@NonNull String replyToken, @NonNull String message) {
@@ -200,4 +151,48 @@ public class LineBotController {
         }
     }
 
+    private void system(String... args) {
+        ProcessBuilder processBuilder = new ProcessBuilder(args);
+        try {
+            Process start = processBuilder.start();
+            int i = start.waitFor();
+            log.info("result: {} => {}", Arrays.toString(args), i);
+        } catch (InterruptedException e) {
+            log.info("Interrupted", e);
+            Thread.currentThread().interrupt();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static DownloadedContent saveContent(String ext, MessageContentResponse response) {
+        log.info("Content-type: {}", response);
+        DownloadedContent tempFile = createTempFile(ext);
+        try (OutputStream outputStream = Files.newOutputStream(tempFile.path)) {
+            ByteStreams.copy(response.getStream(), outputStream);
+            log.info("Save {}: {}", ext, tempFile);
+            return tempFile;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static DownloadedContent createTempFile(String ext) {
+        String fileName = LocalDateTime.now() + "-" + UUID.randomUUID().toString() + "." + ext;
+        Path tempFile = Application.downloadedContentDir.resolve(fileName);
+        tempFile.toFile().deleteOnExit();
+        return new DownloadedContent(tempFile, createUri("/downloaded/" + tempFile.getFileName()));
+
+    }
+
+    private static String createUri(String path) {
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(path).toUriString();
+    }
+
+    @Value
+    public static class DownloadedContent {
+        Path path;
+        String uri;
+    }
 }
