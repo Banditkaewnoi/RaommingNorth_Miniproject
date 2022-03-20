bf0fe5713673644b2a1cab510bb33611182cae54
Bandit Kaewnoi <banditk64@nu.ac.th>
2022-03-20 20:50:59 +0700

    edit v

--- src/main/java/com/banditworkshop2/linebot/LineBotController.java
+++ src/main/java/com/banditworkshop2/linebot/LineBotController.java
@@ -126,23 +126,13 @@ public class LineBotController {
             case "พะเยา": {
                 String userId = event.getSource().getUserId();
 
-                if (userId != null) {
-                    lineMessagingClient.getProfile(userId)
-                            .whenComplete((phayao, throwable) -> {
-                                if (throwable != null) {
-                                    this.replyText(replyToken, throwable.getMessage());
-                                    return;
-                                }
-                                this.reply(replyToken, Arrays.asList(
-                                        new TextMessage(""),
-                                        new TextMessage(
-                                                "https://www.google.com/maps/place/Phayao/@19.2672932,99.0371395,8z/data=!3m1!4b1!4m5!3m4!1s0x30d82120f437301b:0x195a4b21f6c96a8c!8m2!3d19.2154367!4d100.2023692")));
+                this.reply(replyToken, Arrays.asList(
+                        new TextMessage(""),
+                        new TextMessage(
+                                "https://www.google.com/maps/place/Phayao/@19.2672932,99.0371395,8z/data=!3m1!4b1!4m5!3m4!1s0x30d82120f437301b:0x195a4b21f6c96a8c!8m2!3d19.2154367!4d100.2023692")));
 
-                            });
-                }
-                break;
             }
-
+                ;
         }
     }
 
--- src/main/java/com/banditworkshop2/linebot/RomingNorth.java
+++ src/main/java/com/banditworkshop2/linebot/RomingNorth.java
@@ -22,55 +22,23 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class RomingNorth extends LineBotController {
-    private void handleTextContent(String replyToken, Event event, TextMessageContent content) {
+    private void Phayao(String replyToken, TextMessageContent content) {
         String text = content.getText();
 
         log.info("Got text message from %s : %s", replyToken, text);
 
         switch (text) {
-            case "Profile": {
-                String userId = event.getSource().getUserId();
-                if (userId != null) {
-                    lineMessagingClient.getProfile(userId)
-                            .whenComplete((profile, throwable) -> {
-                                if (throwable != null) {
-                                    this.replyText(replyToken, throwable.getMessage());
-                                    return;
-                                }
-                                this.reply(replyToken, Arrays.asList(
-                                        new TextMessage("Display name: " + profile.getDisplayName()),
-                                        new TextMessage("Status message: " + profile.getStatusMessage()),
-                                        new TextMessage("User ID: " + profile.getUserId())));
-                            });
-                }
-                break;
-            }
-            default:
-                log.info("Return echo message %s : %s", replyToken, text);
-                this.replyText(replyToken,
-                        "กรุณาพิมพ์ชื่อจังหวัดที่อยู่ในภาคเหนือด้วยนะครับพิมพ์ให้ถูกด้วยเน้อ :D " + text);
 
             case "พะเยา": {
-                String userId = event.getSource().getUserId();
+                this.reply(replyToken, Arrays.asList(
+                        new TextMessage("จังหวัดพะเยาพะเยา"),
+                        new TextMessage(
+                                "https://www.google.com/maps/place/Phayao/@19.2672932,99.0371395,8z/data=!3m1!4b1!4m5!3m4!1s0x30d82120f437301b:0x195a4b21f6c96a8c!8m2!3d19.2154367!4d100.2023692")));
 
-                if (userId != null) {
-                    lineMessagingClient.getProfile(userId)
-                            .whenComplete((phayao, throwable) -> {
-                                if (throwable != null) {
-                                    this.replyText(replyToken, throwable.getMessage());
-                                    return;
-                                }
-                                this.reply(replyToken, Arrays.asList(
-                                        new TextMessage("จังหวัดพะเยาพะเยา"),
-                                        new TextMessage(
-                                                "https://www.google.com/maps/place/Phayao/@19.2672932,99.0371395,8z/data=!3m1!4b1!4m5!3m4!1s0x30d82120f437301b:0x195a4b21f6c96a8c!8m2!3d19.2154367!4d100.2023692")));
-
-                            });
-                }
-                break;
             }
-
+                ;
         }
+
     }
 
     protected void replyText(@NonNull String replyToken, @NonNull String message) {
@@ -95,6 +63,11 @@ public class RomingNorth extends LineBotController {
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
+    }
+    @Value
+    public static class DownloadedContent {
+        Path path;
+        String uri;
     }
 
 }
