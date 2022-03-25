bf671cdc2a935ba19798f23dac7329d24606979e
Bandit Kaewnoi <banditk64@nu.ac.th>
2022-03-25 15:48:35 +0700

    loA

---
+++ src/main/java/com/banditworkshop2/linebot/ChiangRaiFlex.java
@@ -0,0 +1,127 @@
+package com.banditworkshop2.linebot;
+import com.linecorp.bot.model.action.URIAction;
+import com.linecorp.bot.model.message.FlexMessage;
+import com.linecorp.bot.model.message.flex.component.*;
+import com.linecorp.bot.model.message.flex.component.Button.ButtonHeight;
+import com.linecorp.bot.model.message.flex.component.Image.ImageAspectMode;
+import com.linecorp.bot.model.message.flex.component.Image.ImageAspectRatio;
+import com.linecorp.bot.model.message.flex.container.Bubble;
+import com.linecorp.bot.model.message.flex.unit.FlexFontSize;
+import com.linecorp.bot.model.message.flex.unit.FlexLayout;
+import com.linecorp.bot.model.message.flex.unit.FlexMarginSize;
+
+import java.util.function.Supplier;
+
+import static java.util.Arrays.asList;
+
+public class ChiangRaiFlex implements Supplier<FlexMessage> {
+    @Override
+    public FlexMessage get() {
+        final Image heroBlock = createHeroBlock();
+        final Box bodyBlock = createBodyBlock();
+        final Box footerBlock = createFooterBlock();
+
+        final Bubble bubbleContainer = Bubble.builder()
+                .hero(heroBlock)
+                .body(bodyBlock)
+                .footer(footerBlock)
+                .build();
+        return new FlexMessage("Chiang rai", bubbleContainer);
+    }
+
+    private Image createHeroBlock() {
+        return Image.builder()
+                .url("https://cms.dmpcdn.com/travel/2021/07/27/f4aa6420-eeb9-11eb-8793-61b2b4d8b27c_original.jpg")
+                .size(Image.ImageSize.FULL_WIDTH)
+                .aspectRatio(ImageAspectRatio.R20TO13)
+                .aspectMode(ImageAspectMode.Cover)
+                .action(new URIAction("label", "https://cms.dmpcdn.com/travel/2021/07/27/f4aa6420-eeb9-11eb-8793-61b2b4d8b27c_original.jpg"))
+                .build();
+    }
+
+    private Box createBodyBlock() {
+            
+        final Text title = Text.builder()
+                .text("เหมืองแม่เมาะ")
+                .color("#075673")
+                .weight(Text.TextWeight.BOLD)
+                .size(FlexFontSize.XL)
+                .build();
+
+        final Box info = createInfoBox();
+                
+
+        return Box.builder()
+                .layout(FlexLayout.VERTICAL)
+                .contents(asList(title, info))
+                .build();
+    }
+
+    private Box createInfoBox() {
+        final Box place = Box.builder()
+                .layout(FlexLayout.BASELINE)
+                .spacing(FlexMarginSize.SM)
+                .contents(asList(
+                        Text.builder()
+                            .text("Place")
+                            .color("#aaaaaa")
+                            .size(FlexFontSize.SM)
+                            .flex(1)
+                            .build(),
+                        Text.builder()
+                            .text("อ.แม่เมาะ, จ.ลำปาง")
+                            .wrap(true)
+                            .color("#666666")
+                            .flex(5)
+                            .build()
+                )).build();
+        final Box time = Box.builder()
+                .layout(FlexLayout.BASELINE)
+                .spacing(FlexMarginSize.SM)
+                .contents(asList(
+                        Text.builder().text("Time")
+                            .color("#aaaaaa")
+                            .size(FlexFontSize.SM)
+                            .flex(1)
+                            .build(),
+                        Text.builder()
+                            .text("เปิดทุกวัน (สามารถเช็คเวลาในแต่ละโซนได้ที่ กฟผ.แม่เมาะ)")
+                            .wrap(true)
+                            .color("#666666")
+                            .size(FlexFontSize.SM)
+                            .flex(5)
+                            .build()
+                )).build();
+        return Box.builder()
+                .layout(FlexLayout.VERTICAL)
+                .margin(FlexMarginSize.LG)
+                .spacing(FlexMarginSize.SM)
+                .contents(asList(place, time))
+                .build();
+    }
+
+   
+    private Box createFooterBlock() {
+        final Spacer spacer = Spacer.builder().size(FlexMarginSize.SM).build();
+        final Button websiteAction = Button.builder()
+                .style(Button.ButtonStyle.LINK)
+                .height(ButtonHeight.SMALL)
+                .color("#000000")
+                .action(new URIAction("ข้อมูล", "https://travel.trueid.net/detail/Ylyb279EP1Ra"))
+                .build();
+        final Button ortherAction = Button.builder()
+                .style(Button.ButtonStyle.LINK)
+                .height(ButtonHeight.MEDIUM)
+                .action(new URIAction("สถานที่ท่องเที่ยวเพิ่มเติม", "https://travel.trueid.net/detail/jXqlkRaLX5p"))
+                .color("#000000")
+                
+                .build();
+        final Separator separator = Separator.builder().build();
+        return Box.builder()
+                .layout(FlexLayout.VERTICAL)
+                .spacing(FlexMarginSize.SM)
+                .contents(asList(spacer, websiteAction, separator, ortherAction))
+                .build();
+
+    }
+}
--- src/main/java/com/banditworkshop2/linebot/PhayaoFlex.java
+++ src/main/java/com/banditworkshop2/linebot/PhayaoFlex.java
@@ -16,111 +16,120 @@ import java.util.function.Supplier;
 import static java.util.Arrays.asList;
 
 public class PhayaoFlex implements Supplier<FlexMessage> {
-    @Override
-    public FlexMessage get() {
-        final Image heroBlock = createHeroBlock();
-        final Box bodyBlock = createBodyBlock();
-        final Box footerBlock = createFooterBlock();
+        @Override
+        public FlexMessage get() {
+                final Image heroBlock = createHeroBlock();
+                final Box bodyBlock = createBodyBlock();
+                final Box footerBlock = createFooterBlock();
 
-        final Bubble bubbleContainer = Bubble.builder()
-                .hero(heroBlock)
-                .body(bodyBlock)
-                .footer(footerBlock)
-                .build();
-        return new FlexMessage("Phayao", bubbleContainer);
-    }
+                final Bubble bubbleContainer = Bubble.builder()
+                                .hero(heroBlock)
+                                .body(bodyBlock)
+                                .footer(footerBlock)
+                                .build();
+                return new FlexMessage("Phayao", bubbleContainer);
+        }
 
-    private Image createHeroBlock() {
-        return Image.builder()
-                .url("https://www.museumthailand.com/upload/evidence/1500956664_99367.jpg")
-                .size(Image.ImageSize.FULL_WIDTH)
-                .aspectRatio(ImageAspectRatio.R20TO13)
-                .aspectMode(ImageAspectMode.Cover)
-                .action(new URIAction("label", "https://www.museumthailand.com/upload/evidence/1500956664_99367.jpg"))
-                .build();
-    }
+        private Image createHeroBlock() {
+                return Image.builder()
+                                .url("https://www.museumthailand.com/upload/evidence/1500956664_99367.jpg")
+                                .size(Image.ImageSize.FULL_WIDTH)
+                                .aspectRatio(ImageAspectRatio.R20TO13)
+                                .aspectMode(ImageAspectMode.Cover)
+                                .action(new URIAction("label",
+                                                "https://www.museumthailand.com/upload/evidence/1500956664_99367.jpg"))
+                                .build();
+        }
 
-    private Box createBodyBlock() {
-        final Text title = Text.builder()
-                .text("กว๊านพะเยา")
-                .color("#052638")
-                .weight(Text.TextWeight.BOLD)
-                .size(FlexFontSize.XL)
-                .build();
+        private Box createBodyBlock() {
 
-        final Box info = createInfoBox();
+                final Text title = Text.builder()
+                                .text("กว๊านพะเยา")
+                                .color("#075673")
+                                .weight(Text.TextWeight.BOLD)
+                                .size(FlexFontSize.XL)
+                                .build();
 
-        return Box.builder()
-                .layout(FlexLayout.VERTICAL)
-                .contents(asList(title, info))
-                .build();
-    }
+                final Box info = createInfoBox();
 
-    private Box createInfoBox() {
-        final Box place = Box.builder()
-                .layout(FlexLayout.BASELINE)
-                .spacing(FlexMarginSize.SM)
-                .contents(asList(
-                        Text.builder()
-                            .text("Place")
-                            .color("#aaaaaa")
-                            .size(FlexFontSize.SM)
-                            .flex(1)
-                            .build(),
-                        Text.builder()
-                            .text("เมืองพะเยา, จังหวัดพะเยา")
-                            .wrap(true)
-                            .color("#666666")
-                            .flex(5)
-                            .build()
-                )).build();
-        final Box time = Box.builder()
-                .layout(FlexLayout.BASELINE)
-                .spacing(FlexMarginSize.SM)
-                .contents(asList(
-                        Text.builder().text("Time")
-                            .color("#aaaaaa")
-                            .size(FlexFontSize.SM)
-                            .flex(1)
-                            .build(),
-                        Text.builder()
-                            .text("เปิดตลอดเวลา")
-                            .wrap(true)
-                            .color("#666666")
-                            .size(FlexFontSize.SM)
-                            .flex(5)
-                            .build()
-                )).build();
-        return Box.builder()
-                .layout(FlexLayout.VERTICAL)
-                .margin(FlexMarginSize.LG)
-                .spacing(FlexMarginSize.SM)
-                .contents(asList(place, time))
-                .build();
-    }
+                return Box.builder()
+                                .layout(FlexLayout.VERTICAL)
+                                .contents(asList(title, info))
+                                .build();
+        }
 
-   
-    private Box createFooterBlock() {
-        final Spacer spacer = Spacer.builder().size(FlexMarginSize.SM).build();
-        final Button websiteAction = Button.builder()
-                .style(Button.ButtonStyle.LINK)
-                .height(ButtonHeight.SMALL)
-                .color("#000000")
-                .action(new URIAction("ข้อมูล", "https://www.museumthailand.com/th/1725/storytelling/%E0%B8%81%E0%B8%A7%E0%B9%8A%E0%B8%B2%E0%B8%99%E0%B8%9E%E0%B8%B0%E0%B9%80%E0%B8%A2%E0%B8%B2/"))
-                .build();
-        final Button ortherAction = Button.builder()
-                .style(Button.ButtonStyle.LINK)
-                .height(ButtonHeight.MEDIUM)
-                .action(new URIAction("สถานที่ท่องเที่ยวเพิ่มเติม", "https://travel.trueid.net/detail/X9zwqekRaBjZ"))
-                .color("#000000")
-                
-                .build();
-        final Separator separator = Separator.builder().build();
-        return Box.builder()
-                .layout(FlexLayout.VERTICAL)
-                .spacing(FlexMarginSize.SM)
-                .contents(asList(spacer, websiteAction, separator, ortherAction))
-                .build();
+        private Box createInfoBox() {
+                final Box place = Box.builder()
+                                .layout(FlexLayout.BASELINE)
+                                .spacing(FlexMarginSize.SM)
+                                .contents(asList(
+                                                Text.builder()
+                                                                .text("Place")
+                                                                .color("#aaaaaa")
+                                                                .size(FlexFontSize.SM)
+                                                                .flex(1)
+                                                                .build(),
+                                                Text.builder()
+                                                                .text("อ.เมืองพะเยา, จ.พะเยา")
+                                                                .wrap(true)
+                                                                .color("#666666")
+                                                                .flex(5)
+                                                                .build()))
+                                .build();
+                final Box time = Box.builder()
+                                .layout(FlexLayout.BASELINE)
+                                .spacing(FlexMarginSize.SM)
+                                .contents(asList(
+                                                Text.builder().text("Time")
+                                                                .color("#aaaaaa")
+                                                                .size(FlexFontSize.SM)
+                                                                .flex(1)
+                                                                .build(),
+                                                Text.builder()
+                                                                .text("เปิดตลอดเวลา")
+                                                                .wrap(true)
+                                                                .color("#666666")
+                                                                .size(FlexFontSize.SM)
+                                                                .flex(5)
+                                                                .build()))
+                                .build();
+                return Box.builder()
+                                .layout(FlexLayout.VERTICAL)
+                                .margin(FlexMarginSize.LG)
+                                .spacing(FlexMarginSize.SM)
+                                .contents(asList(place, time))
+                                .build();
+        }
 
-    }
+        private Box createFooterBlock() {
+                final Spacer spacer = Spacer.builder().size(FlexMarginSize.SM).build();
+                final Button websiteAction = Button.builder()
+                                .style(Button.ButtonStyle.LINK)
+                                .height(ButtonHeight.SMALL)
+                                .color("#000000")
+                                .action(new URIAction("ข้อมูล",
+                                                "https://www.museumthailand.com/th/1725/storytelling/%E0%B8%81%E0%B8%A7%E0%B9%8A%E0%B8%B2%E0%B8%99%E0%B8%9E%E0%B8%B0%E0%B9%80%E0%B8%A2%E0%B8%B2/"))
+                                .build();
+                final Button ortherAction = Button.builder()
+                                .style(Button.ButtonStyle.LINK)
+                                .height(ButtonHeight.SMALL)
+                                .action(new URIAction("สถานที่ท่องเที่ยวเพิ่มเติม",
+                                                "https://travel.trueid.net/detail/X9zwqekRaBjZ"))
+                                .color("#000000")
+                                .build();
+                final Button locAction = Button.builder()
+                                .style(Button.ButtonStyle.LINK)
+                                .height(ButtonHeight.SMALL)
+                                .action(new URIAction("สถานที่ท่องเที่ยวเพิ่มเติม",
+                                                "https://travel.trueid.net/detail/X9zwqekRaBjZ"))
+                                .color("#000000")
+                                .build();
+                final Separator separator = Separator.builder().build();
+                return Box.builder()
+                                .layout(FlexLayout.VERTICAL)
+                                .spacing(FlexMarginSize.SM)
+                                .contents(asList(spacer, websiteAction,separator,locAction,separator, ortherAction))
+                                .build();
+
+        }
 }
